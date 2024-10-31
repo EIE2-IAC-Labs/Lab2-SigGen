7d003b05c489fd85eb3c8bdfda7b80cb2982ddf8
gaoziqian123 <zg1223@ic.ac.uk>
2024-10-31 22:46:31 +0800

    Test Commit

---
+++ task1/doit.sh
@@ -0,0 +1,5 @@
+rm -rf obj_dir
+rm -f sinegena.vcd
+verilator -Wall --cc --trace sinegen.sv --exe sinegen_tb.cpp
+make -j -C obj_dir/ -f Vsinegen.mk Vsinegen
+obj_dir/Vsinegen
---
+++ task1/rom.sv
@@ -0,0 +1,20 @@
+module rom #(
+    parameter ADDRESS_WIDTH=8,
+              DATA_WIDTH=8
+)(
+    input logic clk,
+    input logic [ADDRESS_WIDTH-1:0] addr,
+    output logic [DATA_WIDTH-1:0] dout,
+    output logic [DATA_WIDTH-1:0] dout2
+    );
+logic [DATA_WIDTH-1:0] rom_array[2**ADDRESS_WIDTH-1:0];
+
+initial begin
+        $display("Loading rom.");
+        $readmemh("sinerom.mem",rom_array);
+end;
+always_ff@(posedge clk)
+    dout<=rom_array[addr];
+always_ff@(posedge clk)
+    dout2<=rom_array[addr+64];
+endmodule
